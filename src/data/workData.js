a9c2526f496e5dd428e54caa50237dd9a09bee8d
skaroweb <skaroweb@gmail.com>
2023-11-17 13:42:47 +0530

    eleventh

--- src/data/workData.js
+++ src/data/workData.js
@@ -4,6 +4,15 @@ import Zunotrix from "../assets/images/portfolio/zunotrix.png";
 import Hotdealsbazaar from "../assets/images/portfolio/hotdealsbazaar.png";
 import Bimand from "../assets/images/portfolio/bimand.png";
 import Xtecpro from "../assets/images/portfolio/xtecpro.png";
+import Kxarquitectura from "../assets/images/portfolio/kxarquitectura.png";
+import Hakamtcs from "../assets/images/portfolio/hakamtcs.png";
+import Paisavasooldeals from "../assets/images/portfolio/paisavasooldeals.png";
+import Seoengine from "../assets/images/portfolio/seoengine.png";
+import Travelblog from "../assets/images/portfolio/travelblog.png";
+import Instaengagement from "../assets/images/portfolio/insta-engagement.png";
+import Sapelectrical from "../assets/images/portfolio/sapelectrical.png";
+import Aathira from "../assets/images/portfolio/aathira.png";
+import Drmachinery from "../assets/images/portfolio/drmachinery.png";
 
 let WorkData = [
   {
@@ -33,15 +42,6 @@ let WorkData = [
 
   {
     id: 3,
-    projectName: "Gummies",
-    projectDetails:
-      "Looking for a weight loss supplement that actually helps you lose stubborn weight & body fat? Well, you can stop your search right here – because Total Keto ACV + BCB Gummies from Good Health Labs are the real deal. After trying out a bunch of brands, these gummies stood out as the champions, delivering quick and safe weight loss for everyone in our testing crew, including those with some serious weight loss goals.",
-    projectTechnologies: ["HTML", "CSS", "JavaScript"],
-    imageURL: Gummies,
-  },
-
-  {
-    id: 4,
     projectName: "Zunotrix",
     projectDetails:
       "Currently I am a front-end developer and my goal is to become MERN stack developer. I created my portfolio website which contains all my details. Other people can see about me, my skills, projects I have done and can also send me a message. I will use this website for professional development to get hired in a best software company and also to get freelancing projects. I will always update it gradually.",
@@ -50,7 +50,7 @@ let WorkData = [
   },
 
   {
-    id: 5,
+    id: 4,
     projectName: "Bimand",
     projectDetails:
       "BIMAND's main objective is to introduce everything that the BIM methodology entails in the Principality of Andorra, offering advice , training and service provision . Our team has more than 8 years of experience working in BIM methodology. During all this time we have not stopped training , obtaining postgraduate degrees and professional certifications , among which the BIM Masters and the Revit ACP stand out . At a professional level, we have developed more than 40 architecture, structure and installation projects; we have provided training to more than 50 professionals in the AEC sector and; we have offered services such as implementations , energy analyzes and BIM models .",
@@ -59,12 +59,102 @@ let WorkData = [
   },
 
   {
-    id: 6,
+    id: 5,
     projectName: "Xtecpro",
     projectDetails:
       "XTecPro is a service-oriented Information Technology organization that provides solutions and support of Enterprise Business Applications and B2B-EDI to Customer's need and requirements. We also engage ourselves in providing database support for various business applications",
     projectTechnologies: ["HTML", "CSS", "PHP", "jQuery", "Bootstrap"],
     imageURL: Xtecpro,
+  },
+
+  {
+    id: 6,
+    projectName: "Kxarquitectura",
+    projectDetails:
+      "Architecture studio that merge design expertise and new technologies to achieve your goals",
+    projectTechnologies: ["HTML", "CSS", "jQuery"],
+    imageURL: Kxarquitectura,
+  },
+
+  {
+    id: 7,
+    projectName: "Hakamtcs",
+    projectDetails:
+      "We are provider and Supplier of longer shelf life and supreme quality cocunut in Qatar, which procured from different farm in pollachi india, our products are in huge demand amongst our clients due to their worthier equality, we provide facility to our customers to customise of packing.",
+    projectTechnologies: ["HTML", "CSS", "jQuery"],
+    imageURL: Hakamtcs,
+  },
+
+  {
+    id: 8,
+    projectName: "Paisavasooldeals",
+    projectDetails:
+      "Welcome to paisavasooldeals.com, your number one source for all things Discounts,Best Deals.We're dedicated to provide you the very best of products through our third party sites at a very minimal price, with a focus on Originality, customer service and uniqueness",
+    projectTechnologies: ["HTML", "CSS", "JavaScript", "Wordpress"],
+    imageURL: Paisavasooldeals,
+  },
+
+  {
+    id: 9,
+    projectName: "Insta Engagement",
+    projectDetails:
+      "Insta-Engagement is owned and operated by Review & Views a digital consulting firm. Insta-Engagement is an organic Instagram engagement and growth service that brings you the growth techniques to successfully grow any Instagram account.",
+    projectTechnologies: ["HTML", "CSS", "jQuery", "Wordpress"],
+    imageURL: Instaengagement,
+  },
+
+  {
+    id: 10,
+    projectName: "Sap electrical",
+    projectDetails:
+      "SAP Electrical are experienced LV & HV cable jointing contractors providing a range of cable jointing solutions.",
+    projectTechnologies: ["HTML", "CSS", "Wordpress", "jQuery"],
+    imageURL: Sapelectrical,
+  },
+
+  {
+    id: 11,
+    projectName: "Seoengine",
+    projectDetails:
+      "SEO stands for search engine optimization. It is the process of getting traffic from the free, organic, editorial or natural search results on search engines.",
+    projectTechnologies: ["HTML", "CSS", "jQuery", "Wordpress"],
+    imageURL: Seoengine,
+  },
+
+  {
+    id: 12,
+    projectName: "Gummies",
+    projectDetails:
+      "Looking for a weight loss supplement that actually helps you lose stubborn weight & body fat? Well, you can stop your search right here – because Total Keto ACV + BCB Gummies from Good Health Labs are the real deal. After trying out a bunch of brands, these gummies stood out as the champions, delivering quick and safe weight loss for everyone in our testing crew, including those with some serious weight loss goals.",
+    projectTechnologies: ["HTML", "CSS", "JavaScript"],
+    imageURL: Gummies,
+  },
+
+  {
+    id: 13,
+    projectName: "Travel Blog",
+    projectDetails:
+      "A captivating travel blog entices readers with vivid storytelling, transporting them to exotic destinations. Engaging narratives, complemented by stunning visuals, create a sensory experience. Practical tips and itineraries aid fellow travelers, while personal anecdotes add authenticity. Insightful cultural observations and local cuisine highlights provide a well-rounded perspective. Incorporating a touch of humor or unique insights adds flair. ",
+    projectTechnologies: ["HTML", "CSS", "PHP"],
+    imageURL: Travelblog,
+  },
+
+  {
+    id: 14,
+    projectName: "Aathira",
+    projectDetails:
+      "We, Aathira Silver Palace, situated at Erode, Tamil Nadu connects you to the world’s most luxurious jewellery ensuring a handpicked edit of the finest jewels. We believe that buying a piece of luxurious jewellery is experience so we ensure to provide best quality jewellery. We are a fine jewellery marketplace, where you can buy from a wide range of our jewellery collection. We are grateful to be trusted by number of happy customers.",
+    projectTechnologies: ["HTML", "CSS", "jQuery"],
+    imageURL: Aathira,
+  },
+
+  {
+    id: 15,
+    projectName: "D&R Machinery",
+    projectDetails:
+      "D&R Machinery are professional confectionery machine manufacturers with over 15 years in the business operating at the highest industry standards. We’ve built a reputation on engineering, designing and delivering high performance total-solution confectionery machinery to clients with absolute precision around the world.",
+    projectTechnologies: ["HTML", "CSS", "jQuery"],
+    imageURL: Drmachinery,
   },
 ];
 
