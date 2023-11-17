cc119a9d43172a8b3617aacd78aa7e694b742f6e
skaroweb <skaroweb@gmail.com>
2023-11-17 16:26:57 +0530

    thirteenth

--- src/data/socialData.js
+++ src/data/socialData.js
@@ -1,54 +1,49 @@
-import {
-  faFacebookSquare,
-  faLinkedin,
-  faGithubSquare,
-  faTwitterSquare,
-  faInstagramSquare,
-} from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+// import {
+//   faFacebookSquare,
+//   faLinkedin,
+//   faGithubSquare,
+//   faTwitterSquare,
+//   faInstagramSquare,
+// } from "@fortawesome/free-brands-svg-icons";
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 let SocialData = [
-  {
-    socialMediaID: 1,
-    socialMediaName: "Facebook",
-    socialMediaIcon: faFacebookSquare,
-    socialMediaLink: "https://www.facebook.com/",
-  },
-
-  {
-    socialMediaID: 2,
-    socialMediaName: "Linkedin",
-    socialMediaIcon: faLinkedin,
-    socialMediaLink: "https://www.linkedin.com/",
-  },
-
-  {
-    socialMediaID: 3,
-    socialMediaName: "Github",
-    socialMediaIcon: faGithubSquare,
-    socialMediaLink: "https://github.com/",
-  },
-
-  {
-    socialMediaID: 1,
-    socialMediaName: "Twitter",
-    socialMediaIcon: faTwitterSquare,
-    socialMediaLink: "https://www.twitter.com/",
-  },
-
-  {
-    socialMediaID: 2,
-    socialMediaName: "Instagram",
-    socialMediaIcon: faInstagramSquare,
-    socialMediaLink: "https://www.instagram.com/",
-  },
-
-  {
-    socialMediaID: 3,
-    socialMediaName: "Email",
-    socialMediaIcon: faEnvelope,
-    socialMediaLink: "mailto:xyz@yahoo.com",
-  },
+  // {
+  //   socialMediaID: 1,
+  //   socialMediaName: "Facebook",
+  //   socialMediaIcon: faFacebookSquare,
+  //   socialMediaLink: "https://www.facebook.com/",
+  // },
+  // {
+  //   socialMediaID: 2,
+  //   socialMediaName: "Linkedin",
+  //   socialMediaIcon: faLinkedin,
+  //   socialMediaLink: "https://www.linkedin.com/",
+  // },
+  // {
+  //   socialMediaID: 3,
+  //   socialMediaName: "Github",
+  //   socialMediaIcon: faGithubSquare,
+  //   socialMediaLink: "https://github.com/",
+  // },
+  // {
+  //   socialMediaID: 4,
+  //   socialMediaName: "Twitter",
+  //   socialMediaIcon: faTwitterSquare,
+  //   socialMediaLink: "https://www.twitter.com/",
+  // },
+  // {
+  //   socialMediaID: 5,
+  //   socialMediaName: "Instagram",
+  //   socialMediaIcon: faInstagramSquare,
+  //   socialMediaLink: "https://www.instagram.com/",
+  // },
+  // {
+  //   socialMediaID: 6,
+  //   socialMediaName: "Email",
+  //   socialMediaIcon: faEnvelope,
+  //   socialMediaLink: "mailto:xyz@yahoo.com",
+  // },
 ];
 
 export default SocialData;
